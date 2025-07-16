76f1e3094943da7222373c6d2d13ef2b9c3db0a0
tikrack <tikrackcode@gmail.com>
2025-07-17 00:32:56 +0330

    feat(validation): validation for command

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -1,5 +1,17 @@
+import logger from "./helpers/logger.js";
+
 const bootstrap = (run_argv) => {
-    console.log(run_argv)
+    const accepted_commands = ["dev", "build"];
+
+    if (!run_argv || !accepted_commands.includes(run_argv)) logger("Run error => Check the running commands", "error")
+
+    if (run_argv === "dev") {
+
+    }
+
+    if (run_argv === "build") {
+
+    }
 }
 
 export default bootstrap
