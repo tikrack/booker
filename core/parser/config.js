4c4ebe240decc3135715801cc088c193e434010f
tikrack <tikrackcode@gmail.com>
2025-07-16 12:44:16 +0330

    feat(vite)

--- core/parser/config.js
+++
@@ -1,9 +0,0 @@
-import readFile from "../helpers/readFile.js";
-
-const config = async () => {
-    return await readFile("book.config.json", {
-        json: true
-    });
-}
-
-export default config
